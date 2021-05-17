15e6820077c813f3d6fe7ad2e2d1b1d609913276
Oleuua <Oleuua@gmail.com>
2021-05-17 22:52:21 +0300

    Initial commit

---
+++ src/com/Oleuua/Game.java
@@ -0,0 +1,10 @@
+package com.Oleuua;
+
+public class Game {
+
+    public static final ColorScheme COLORS = new ColorScheme();
+    public static final Window WINDOW = new Window("Course'овая работа");
+    public static final Controls CONTROLS = new Controls();
+    public static final Board BOARD = new Board(4);
+
+}
---
+++ src/com/Oleuua/Main.java
@@ -0,0 +1,11 @@
+package com.Oleuua;
+import java.util.Scanner;
+public class Main {
+
+    public static void main(String[] args) {
+
+        new Game();
+
+    }
+
+}
